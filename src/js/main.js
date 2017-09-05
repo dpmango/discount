900446581e5fe6cbbd204590955d27ad5e3dc005
dpmango <xs290@me.com>
2017-09-05 16:38:34 +0300

    slider fix

--- src/js/main.js
+++ src/js/main.js
@@ -41,30 +41,6 @@ $(document).ready(function(){
         return false;
 	});
 
-  // HEADER SCROLL
-  // add .header-static for .page or body
-  // to disable sticky header
-  if ( $('.header-static').length == 0 ){
-    _window.scrolled(10, function() { // scrolled is a constructor for scroll delay listener
-      var vScroll = _window.scrollTop();
-      var header = $('.header').not('.header--static');
-      var headerHeight = header.height();
-      var heroHeight = $('.hero').outerHeight() - headerHeight;
-
-      if ( vScroll > headerHeight ){
-        header.addClass('header--transformed');
-      } else {
-        header.removeClass('header--transformed');
-      }
-
-      if ( vScroll > heroHeight ){
-        header.addClass('header--fixed');
-      } else {
-        header.removeClass('header--fixed');
-      }
-    });
-  }
-
   // HAMBURGER TOGGLER
 
   $('.hamburger').on('click', function(event) {
@@ -128,69 +104,43 @@ $(document).ready(function(){
   // 		}
   // 	});
 
-  // SET ACTIVE CLASS IN HEADER
-  // * could be removed in production and server side rendering
-  // user .active for li instead
-  $('.header__menu li').each(function(i,val){
-    if ( $(val).find('a').attr('href') == window.location.pathname.split('/').pop() ){
-      $(val).addClass('active');
-    } else {
-      $(val).removeClass('active')
-    }
-  });
-
-
-  // VIDEO PLAY
-  $('.promo-video .icon').on('click', function(){
-    $(this).closest('.promo-video').toggleClass('playing');
-    $(this).closest('.promo-video').find('iframe').attr("src", $("iframe").attr("src").replace("autoplay=0", "autoplay=1"));
-  });
-
-
   //////////
   // SLIDERS
   //////////
 
+  $('.hero-slider').on('init', function(event, slick){
+    $('.hero-slide[data-slide=' + 1 + ']').addClass('showing');
+  });
 
+  $('.hero-slider').slick({
+    autoplay: false,
+    autoplaySpeed: 4000,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 400,
+    cssEase: 'ease-in',
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    vertical: true,
+    adaptiveHeight: false
+  });
 
-
-    $('.hero-slider').on('init', function(event, slick){
-      $('.hero-slide[data-slide=' + 1 + ']').addClass('showing');
-    });
-
-    $('.hero-slider').slick({
-      autoplay: false,
-      autoplaySpeed: 4000,
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 400,
-      cssEase: 'ease-in',
-      pauseOnFocus: false,
-      pauseOnHover: false,
-      slidesToShow: 1,
-      vertical: true,
-      adaptiveHeight: false
-    });
-
-    // set active class
-    $('.hero-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-      var slideNumber = currentSlide + 1
-      console.log(slideNumber)
-      $('.hero-slide[data-slide=' + slideNumber + ']').addClass('removing');
-    });
-    $('.hero-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-      $('.hero-slide').removeClass('removing');
-      $('.hero-slide').removeClass('showing');
-      var slideNumber = currentSlide + 1;
-      $('.hero-slide[data-slide=' + slideNumber + ']').addClass('showing');
-    });
-
-
+  // set active class
+  $('.hero-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    var slideNumber = currentSlide + 1
+    console.log(slideNumber)
+    $('.hero-slide[data-slide=' + slideNumber + ']').addClass('removing');
+  });
+  $('.hero-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.hero-slide').removeClass('removing');
+    $('.hero-slide').removeClass('showing');
+    var slideNumber = currentSlide + 1;
+    $('.hero-slide[data-slide=' + slideNumber + ']').addClass('showing');
+  });
 
   // -----
-
-
 
   $('.catalog-slider').slick({
     dots: true,
@@ -232,152 +182,28 @@ $(document).ready(function(){
 
 
   $('.card-slider').slick({
-    infinite: false,
+    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: false,
-    asNavFor: '.card-thumbs'
+    accessibility: false
   });
 
-  $('.card-thumbs').slick({
-    infinite: false,
-    slidesToShow: 3,
-    infinite: false,
-    slidesToScroll: 3,
-    asNavFor: '.card-slider',
-    dots: true,
-    arrows: false,
-    centerMode: true,
-    focusOnSelect: true,
-    variableWidth: true
+  $('.card-thumbs-item').on('click', function(){
+    var slideId = $(this).data('slide') - 1;
+    $('.card-slider').slick('goTo', slideId);
+  })
+
+  $('.card-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.card-thumbs-item').siblings().removeClass('active');
+    var slideId = nextSlide + 1
+    $('.card-thumbs-item[data-slide='+ slideId +']').addClass('active')
   });
-
-  //////////
-  // MODALS
-  //////////
-  // Custom modals
-  // $('*[data-modal]').on('click', function(){
-  //   // remove all active first
-  //   $('.modal').removeClass('opened');
-  //
-  //   // find by id
-  //   var target = $(this).data('modal');
-  //   $('#'+target).addClass('opened');
-  //
-  //   window.location.hash = target;
-  // });
-  //
-  // $('.modal__close').on('click', function(){
-  //   $(this).closest('.modal').removeClass('opened');
-  //   window.location.hash = "";
-  // });
-  //
-  // // CHECK SAVED STATE
-  // if(window.location.hash) {
-  //   var hash = window.location.hash.substring(1);
-  //   $('#'+hash).addClass('opened');
-  // }
-  //
-
-
-  // Magnific Popup
-  // var startWindowScroll = 0;
-  // $('.js-popup').magnificPopup({
-  //   type: 'inline',
-  //   fixedContentPos: true,
-  //   fixedBgPos: true,
-  //   overflowY: 'auto',
-  //   closeBtnInside: true,
-  //   preloader: false,
-  //   midClick: true,
-  //   removalDelay: 300,
-  //   mainClass: 'popup-buble',
-  //   callbacks: {
-  //     beforeOpen: function() {
-  //       // startWindowScroll = _window.scrollTop();
-  //       // $('html').addClass('mfp-helper');
-  //     },
-  //     close: function() {
-  //       // $('html').removeClass('mfp-helper');
-  //       // _window.scrollTop(startWindowScroll);
-  //     }
-  //   }
-  // });
-
-
-  // $('.popup-with-move-anim').magnificPopup({
-  //   type: 'inline',
-  //   fixedContentPos: false,
-  //   fixedBgPos: true,
-  //   overflowY: 'auto',
-  //   closeBtnInside: true,
-  //   preloader: false,
-  //   midClick: true,
-  //   removalDelay: 300,
-  //   mainClass: 'my-mfp-slide-bottom'
-  // });
-  //
-  // $('.popup-gallery').magnificPopup({
-	// 	delegate: 'a',
-	// 	type: 'image',
-	// 	tLoading: 'Loading image #%curr%...',
-	// 	mainClass: 'mfp-img-mobile',
-	// 	gallery: {
-	// 		enabled: true,
-	// 		navigateByImgClick: true,
-	// 		preload: [0,1]
-	// 	},
-	// 	image: {
-	// 		tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-	// 	}
-	// });
-
 
   ////////////
   // UI
   ////////////
-
-  // custom selects
-  $('.ui-select__visible').on('click', function(e){
-    var that = this
-    // hide parents
-    $(this).parent().parent().parent().find('.ui-select__visible').each(function(i,val){
-      if ( !$(val).is($(that)) ){
-        $(val).parent().removeClass('active')
-      }
-    });
-
-    $(this).parent().toggleClass('active');
-  });
-
-  $('.ui-select__dropdown span').on('click', function(){
-    // parse value and toggle active
-    var value = $(this).data('val');
-    if (value){
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
-
-      // set visible
-      $(this).closest('.ui-select').removeClass('active');
-      $(this).closest('.ui-select').find('input').val(value);
-
-      $(this).closest('.ui-select').find('.ui-select__visible span').text(value);
-    }
-
-  });
-
-  // handle outside click
-  $(document).click(function (e) {
-    var container = new Array();
-    container.push($('.ui-select'));
-
-    $.each(container, function(key, value) {
-        if (!$(value).is(e.target) && $(value).has(e.target).length === 0) {
-            $(value).removeClass('active');
-        }
-    });
-  });
 
   // numeric input
   $('.ui-number span').on('click', function(e){
@@ -408,15 +234,7 @@ $(document).ready(function(){
   $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
 
 
-
-
   // Formstyler
-
-
   $('.ui-select, .ui-number').styler();
-
-
-
-
 
 });
